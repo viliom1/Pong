5524a6ba692e8e775da584e11e27157e9398c1dd
viliom1 <viliom1@gmail.com>
2015-10-29 16:55:08 +0200

    Added board

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ import java.io.Console;
 public class Main {
 
     public static void main(String[] args) {
-	    boardPrint();
+	    char[][] board = board();
+        boardPrint(board);
         menuPrint();
         chooseGame();
         singlePlayer();
@@ -17,6 +18,33 @@ public class Main {
 
 
 
+
+    }
+
+    private static char[][] board() {
+
+        char[][] board = new char[23][88];
+        for (int i = 0; i <board.length ; i++) {
+            for (int j = 0; j <board[0].length ; j++) {
+                if ((i > 7 && i < 13 ) && (j == 1 || j == board[0].length-2)){
+                    board[i][j] = '#';
+                }
+                else if (j == 43 && i != 0){
+                    board[i][j] = '|';
+                }
+                else if (i == 0 || i == board.length-1){
+                    board[i][j] = '_';
+                }
+                else if (j == 0 || j == board[0].length-1){
+                    board[i][j] = '|';
+                }
+                else{
+                    board[i][j] = ' ';
+                }
+            }
+
+        }
+        return board;
     }
 
     private static void pause() {
@@ -49,8 +77,15 @@ public class Main {
     }
 
     private static void menuPrint() {
+
     }
 
-    private static void boardPrint() {
+    private static void boardPrint(char[][] board) {
+        for (int i = 0; i <board.length ; i++) {
+            for (int j = 0; j <board[0].length ; j++) {
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
