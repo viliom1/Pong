b90a73c4f33d7f7efadba8ef735f653c99d5005e
iludart <peter.penev@hotmail.com>
2015-11-04 09:50:18 +0200

    Deleted some deprecated code

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
         GameEngine ge = new GameEngine().start();
 
-	    //char[][] board = board();
         //boardPrint(board);
         menuPrint();
         chooseGame();
@@ -19,31 +18,6 @@ public class Main {
 
     }
 
-    private static char[][] board() {
-
-        char[][] board = new char[23][88];
-        for (int i = 0; i <board.length ; i++) {
-            for (int j = 0; j <board[0].length ; j++) {
-                if ((i > 7 && i < 13 ) && (j == 1 || j == board[0].length-2)){
-                    board[i][j] = '#';
-                }
-                else if (j == 43 && i != 0){
-                    board[i][j] = '|';
-                }
-                else if (i == 0 || i == board.length-1){
-                    board[i][j] = '_';
-                }
-                else if (j == 0 || j == board[0].length-1){
-                    board[i][j] = '|';
-                }
-                else{
-                    board[i][j] = ' ';
-                }
-            }
-
-        }
-        return board;
-    }
 
     private static void pause() {
     }
