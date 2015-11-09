4d87828108dd347cebfc8d6c6bad56f826ed131c
iludart <peter.penev@hotmail.com>
2015-11-09 12:36:07 +0200

    added server and client functionality

---
+++ src/Pong/Network/KeyInput.java
@@ -0,0 +1,67 @@
+package Pong.Network;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyInput implements KeyListener {
+
+    class Key{
+        private boolean pressed = false;
+        public boolean isPressed(){
+            return pressed;
+        }
+        public void toggle (boolean isPressed){
+            pressed = isPressed;
+        }
+    }
+
+    public KeyInput(Display frame){
+        frame.getCanvas().addKeyListener(this);
+    }
+    public Key p1Up = new Key();
+    public Key p2Up = new Key();
+    public Key p1Down = new Key();
+    public Key p2Down = new Key();
+    public Key pause = new Key();
+    //for StartMenu
+    public Key up = new Key();
+    public Key down = new Key();
+    public Key choseMode = new Key();
+
+
+    public void keyTyped(KeyEvent e) {
+    }
+    public void keyPressed(KeyEvent e) {
+        toggleKey(e.getKeyCode(),true);
+    }
+    public void keyReleased(KeyEvent e) {
+        toggleKey(e.getKeyCode(),false);
+    }
+    public void toggleKey(int keyCode, boolean isPressed){
+        if (keyCode == KeyEvent.VK_Q || keyCode == KeyEvent.VK_UP){
+            p1Up.toggle(isPressed);
+        }
+        if (keyCode == KeyEvent.VK_A || keyCode == KeyEvent.VK_DOWN) {
+            p1Down.toggle(isPressed);
+        }
+        if (keyCode == KeyEvent.VK_P){
+            p2Up.toggle(isPressed);
+        }
+        if (keyCode == KeyEvent.VK_L){
+            p2Down.toggle(isPressed);
+        }
+        if (keyCode == KeyEvent.VK_ESCAPE){
+            // Not implemented yet
+            pause.toggle(isPressed);
+        }
+        if (keyCode == KeyEvent.VK_UP){
+            up.toggle(isPressed);
+        }
+        if (keyCode == KeyEvent.VK_DOWN){
+            down.toggle(isPressed);
+        }
+        if (keyCode == KeyEvent.VK_ENTER){
+            choseMode.toggle(isPressed);
+        }
+    }
+}
---
+++ src/Pong/Network/Platform.java
@@ -0,0 +1,55 @@
+package Pong.Network;
+
+public class Platform {
+    private int x;
+    private int y;
+    public static final int WIDTH = 10;
+    public static final int HEIGHT = 60;
+    private int speed;
+
+    public Platform (int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.speed = 5;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getSpeed() {return  speed;}
+
+    public void setSpeed(int speed) {this.speed = speed;}
+
+    public int getWidth() {
+        return WIDTH;
+    }
+
+    public int getHeight() {
+        return HEIGHT;
+    }
+
+    public void moveUp() {
+        if (this.getY() - this.getSpeed() >= 10) {
+            this.setY(this.getY() - this.getSpeed());
+        }
+    }
+
+    public void moveDown() {
+        if (this.getY() + this.getSpeed() <= Display.HEIGHT - 70) {
+            this.setY(this.getY() + this.getSpeed());
+        }
+    }
+}
